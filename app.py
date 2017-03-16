9df9a8720f7ef2bc9a9191ce911e8059ff2afd1e
Jean-Marie Saurel <saurel@ipgp.fr>
2017-03-16 15:20:18 +0000

    Squashed commit of the following:
    commit 559637e5f7b339b244a726287e5164ebbec4cf6e
    Author: Jean-Marie Saurel <saurel@ipgp.fr>
    Date:   Thu Mar 16 15:11:45 2017 +0000
    
        Le reste
    
    commit 30e7e205f03ddb84e50c00abaa2846e2b3555ba8
    Author: Jean-Marie Saurel <saurel@ipgp.fr>
    Date:   Thu Mar 16 15:10:20 2017 +0000
    
        Deux commentaires
    
    commit 3a97dc1190f33a73b2f7be5c6a16a82ac90dbe0b
    Author: Jean-Marie Saurel <saurel@ipgp.fr>
    Date:   Thu Mar 16 15:05:05 2017 +0000
    
        Un peu de documentation

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
+# Fonction affichage
 def greetings():
+	"""Salute RESIF people"""
 	print("Hello RESIF people !")
 
+# Fonction begaiement
 def repeat(x, callback):
+	"""Call xtimes callback."""
 	for _ in range(x):
 		callback()
 
@@ -11,3 +15,5 @@ if __name__ == "__main__":
 	repeat(3, greetings)
 
 # Et plaf, un commentaire
+
+# Encore un commentaire
